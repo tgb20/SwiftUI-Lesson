0b5136520424dc4f8e5c65c1af1dddec9392fa75
Jacob Bashista <jacob@orange.haus>
2020-02-22 18:11:06 -0500

    Initial Commit

---
+++ SwiftUI-Lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Lesson
+//
+//  Created by Jacob Bashista on 2/22/20.
+//  Copyright © 2020 Jacob Bashista. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
